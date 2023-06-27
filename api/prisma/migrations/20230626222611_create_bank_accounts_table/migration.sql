8f7ea1e64919c8d8e38736de3ea05383d32ec935
Thyago Nunes <devthyagonunes@gmail.com>
2023-06-26 21:37:57 -0300

    Started/Configurated nest/prisma/docker/postgreSQL :fire

---
+++ api/prisma/migrations/20230626222611_create_bank_accounts_table/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "bank_accounts" (
+    "id" UUID NOT NULL,
+    "userId" UUID NOT NULL,
+    "name" TEXT NOT NULL,
+    "initial_balance" DOUBLE PRECISION NOT NULL,
+    "type" TEXT NOT NULL,
+    "color" TEXT NOT NULL,
+
+    CONSTRAINT "bank_accounts_pkey" PRIMARY KEY ("id")
+);
